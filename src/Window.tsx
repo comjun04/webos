617cc87d8e99bd64295f7afc9238e16db20ac8bc
comjun04 <comjun04@gmail.com>
2023-10-04 21:24:02 +0900

    feat: draggable window

--- src/Window.tsx
+++ src/Window.tsx
@@ -1,52 +1,62 @@
 import { Box, Button, Flex, HStack, Spacer } from '@chakra-ui/react'
 import { FC } from 'react'
+import Draggable from 'react-draggable'
 
 const Window: FC = () => {
   return (
-    <Box w={600} h={400} borderRadius="lg" border="1px" borderColor="gray.300">
-      {/* header */}
-      <Flex
-        width="full"
-        height="8"
-        paddingX="4"
-        borderTopRadius="lg"
-        // boxShadow="lg"
-        fontSize="sm"
-        flexDir="row"
-        alignItems="center"
-        backgroundColor="blue.200"
+    <Draggable handle=".topbar" cancel=".topbar button" bounds="body">
+      <Box
+        w={600}
+        h={400}
+        borderRadius="md"
+        border="1px"
+        borderColor="gray.300"
       >
-        Window Title
-        <Spacer />
-        <HStack spacing={1}>
-          <Button
-            width={6}
-            height={6}
-            backgroundColor="yellow.300"
-            borderRadius="full"
-          >
-            --
-          </Button>
-          <Button
-            width={6}
-            height={6}
-            backgroundColor="green.300"
-            borderRadius="full"
-          >
-            □
-          </Button>
-          <Button
-            width={6}
-            height={6}
-            backgroundColor="red.300"
-            borderRadius="full"
-          >
-            &times;
-          </Button>
-        </HStack>
-      </Flex>
-      <Box>hello world </Box>
-    </Box>
+        {/* header */}
+        <Flex
+          width="full"
+          height="8"
+          paddingX="4"
+          borderTopRadius="md"
+          // boxShadow="lg"
+          fontSize="sm"
+          flexDir="row"
+          alignItems="center"
+          backgroundColor="blue.200"
+          className="topbar"
+        >
+          Window Title
+          <Spacer />
+          <HStack spacing={1}>
+            <Button
+              width={6}
+              height={6}
+              backgroundColor="yellow.300"
+              borderRadius="full"
+            >
+              --
+            </Button>
+            <Button
+              width={6}
+              height={6}
+              backgroundColor="green.300"
+              borderRadius="full"
+            >
+              □
+            </Button>
+            <Button
+              width={6}
+              height={6}
+              backgroundColor="red.300"
+              borderRadius="full"
+            >
+              &times;
+            </Button>
+          </HStack>
+        </Flex>
+        <Box>hello world </Box>
+      </Box>
+    </Draggable>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
+import './style.css'
 
 const { Button } = theme.components
 
