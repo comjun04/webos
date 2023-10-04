078cb7b5ce18639720111d924f50a0e8f708bafd
comjun04 <comjun04@gmail.com>
2023-10-04 23:34:49 +0900

    feat(Window): maximize

--- src/Window.tsx
+++ src/Window.tsx
@@ -1,24 +1,42 @@
 import { Box, Button, Flex, HStack, Spacer, Text } from '@chakra-ui/react'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Draggable from 'react-draggable'
-import { MdClose, MdFullscreen, MdLogoDev, MdMinimize } from 'react-icons/md'
+import {
+  MdClose,
+  MdFullscreen,
+  MdFullscreenExit,
+  MdLogoDev,
+  MdMinimize,
+} from 'react-icons/md'
 
 const Window: FC = () => {
+  const [maximized, setMaximized] = useState(false)
+
+  const handleMaximizeBtnClick = () => {
+    setMaximized(!maximized)
+  }
+
   return (
-    <Draggable handle=".topbar" cancel=".topbar button" bounds="body">
+    <Draggable
+      handle=".topbar"
+      cancel=".topbar button"
+      bounds="body"
+      position={maximized ? { x: 0, y: 0 } : undefined}
+      disabled={maximized}
+    >
       <Box
-        w={600}
-        h={400}
-        borderRadius="md"
+        w={maximized ? 'full' : 600}
+        h={maximized ? '100vh' : 400}
+        borderRadius={maximized ? 'none' : 'md'}
         border="1px"
-        borderColor="gray.300"
+        borderColor="gray.600"
       >
         {/* header */}
         <Flex
           width="full"
           height="8"
           paddingX="4"
-          borderTopRadius="md"
+          borderTopRadius={maximized ? 'none' : 'md'}
           // boxShadow="lg"
           flexDir="row"
           alignItems="center"
@@ -44,8 +62,9 @@ const Window: FC = () => {
               height={6}
               backgroundColor="green.300"
               borderRadius="full"
+              onClick={handleMaximizeBtnClick}
             >
-              <MdFullscreen />
+              {maximized ? <MdFullscreenExit /> : <MdFullscreen />}
             </Button>
             <Button
               width={6}
